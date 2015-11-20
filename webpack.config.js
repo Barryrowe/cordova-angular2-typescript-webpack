0a120cc1498c5663614a5368e25bedbe428d4e20
Barry Rowe <barryrowe@gmail.com>
2015-11-19 21:11:37 -0500

    Add SASS example
    Moved source files to a src directory, and updated webpack to bundle css.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
 var path = require('path');
-var _basePath = path.resolve(__dirname);
+var fs = require('fs');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var _contextPath = path.resolve(__dirname);
+var _sourcePath = path.join(__dirname, 'src');
+
+
+
+var _appEntryPath = path.join(_sourcePath, 'app', 'app.ts');
+var _stylesEntryPath = fs.readdirSync(path.join(_sourcePath, 'styles'))
+						.map((filename) => { return path.join(_sourcePath, 'styles', filename)});
 
 module.exports = {
 	stats: {
 		colors: true,
 		reasons: true
 	},
-	context: _basePath,
+	// context: _contextPath,
 	entry:{
 		'angular2': [
 		 // Angular 2 Deps
@@ -19,9 +29,8 @@ module.exports = {
 	      'angular2/router',
 	      'angular2/http'
 	    ],
-		'app': [
-			'./www/app/app.ts'
-		]
+		'app': './src/app/app.ts',
+		'styles': _stylesEntryPath
 	},
 	output:{
 		path: path.join(__dirname, 'www', 'build', 'js'),
@@ -38,7 +47,16 @@ module.exports = {
 				test:/\.ts$/, 
 				loader:'webpack-typescript',
 				exclude:/node_modules/
+			},
+			{
+				test:/\.scss$/,
+				loader: ExtractTextPlugin.extract("style", ["css", "autoprefixer", "sass"])
 			}
 		]
+	},
+	plugins: [
+		new ExtractTextPlugin("[name].bundle.css")
+	],
+	sassLoader: {		
 	}
 };
